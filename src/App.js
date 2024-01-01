0f44d716d1d91ead2b28b9cec81a8d9b4430fbde
Cameron Wood <camwood23@gmail.com>
2024-01-01 15:27:10 -0500

    Changing location and title

--- src/App.js
+++ src/App.js
@@ -38,12 +38,12 @@ function App() {
       <Box w='100vw' h='100vh' backgroundImage="linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('image0.jpeg')" backgroundPosition="center" backgroundSize="cover" color="#ECECEC">
         <Box w='100%' h='15vh' position='relative' borderBottom='2px' pb={2}>
           <Flex h='full' align="flex-end" justify="center">
-            <Text fontSize={{base:"1.5rem",md:"2rem"}}>SOFTWARE ENGINEER</Text>
+            <Text fontSize={{base:"1.5rem",md:"2rem"}}>SENIOR SOFTWARE ENGINEER</Text>
           </Flex>
         </Box>
         <VStack w='full' h='85vh' alignItems='flex-start' spacing="10vh" px={{base:10,md:35,lg:40}} py={{base:5,md:10}}>
           <Grid w='full' templateColumns='repeat(3,1fr)'>
-            <Text fontSize={responsiveText}>Atlanta, Georgia</Text>
+            <Text fontSize={responsiveText}>Orlando, Florida</Text>
             <Center><Text fontSize={responsiveText}>Computer Science B.S.</Text></Center>
           </Grid>
           <Heading as="h1" ml={{xl:150}} letterSpacing="0.8rem" fontSize={{base:'5rem',md:'10rem','2xl':'13rem'}}>CAMERON<br/>WOOD</Heading>
