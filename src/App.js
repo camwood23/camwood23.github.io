b1c606eb090d6424ccb04e6205a092775d0ffbd9
Cameron Wood <camwood23@gmail.com>
2025-01-26 13:00:32 -0500

    Adding new font and moved some things around

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import '@fontsource/montserrat/400.css'
 import '@fontsource/bebas-neue';
+// Supports weights 400-800
+import '@fontsource-variable/wix-madefor-text';
 import { SiLinkedin } from 'react-icons/si';
+import { FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa';
 import {
   ChakraProvider,
   Box,
@@ -12,12 +14,11 @@ import {
   Center,
   Grid,
   Flex,
-  Spacer,
 } from '@chakra-ui/react';
  
 const fonts = {
   heading: 'Bebas Neue, sans-serif',
-  body: 'Montserrat, sans-serif'
+  body: 'Wix Madefor Text Variable, sans-serif'
 }
 
 const fontSizes = {
@@ -29,28 +30,35 @@ const theme = extendTheme({ fonts, fontSizes })
 const responsiveText = {
   base: '0.75rem',
   md: '1rem',
-  lg: '1.5rem'
+  lg: '1.25rem'
 }
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box w='100vw' h='100vh' backgroundImage="linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url('image0.jpeg')" backgroundPosition="center" backgroundSize="cover" color="#ECECEC">
-        <Box w='100%' h='15vh' position='relative' borderBottom='2px' pb={2}>
-          <Flex h='full' align="flex-end" justify="center">
-            <Text fontSize={{base:"1.5rem",md:"2rem"}}>FULL STACK SOFTWARE ENGINEER</Text>
-          </Flex>
+        <Box w='full' h='15vh' position='relative' borderBottom='2px' pb={5} px={{base:10,md:35,lg:20}} display='flex' flexDirection='column' justifyContent='flex-end'>
+          <Center>
+            <VStack>
+              <Text fontSize={{base:"1.5rem",md:"2rem"}}>FULL STACK SOFTWARE ENGINEER</Text>
+              <Flex>
+                <a href="https://linkedin.com/in/cameron-wood-412708124"><SiLinkedin fontSize={responsiveText} /></a>
+                <a href="mailto:camwood23@gmail.com"><FaEnvelope fontSize={responsiveText} style={{ marginLeft: '20px' }} /></a>
+              </Flex>
+            </VStack>
+          </Center>
         </Box>
-        <VStack w='full' h='85vh' alignItems='flex-start' spacing="10vh" px={{base:10,md:35,lg:40}} py={{base:5,md:10}}>
+        <VStack w='full' h='85vh' alignItems='flex-start' spacing="10vh" px={{base:10,md:35,lg:20}} py={{base:5,md:10}}>
           <Grid w='full' templateColumns='repeat(3,1fr)'>
-            <Text fontSize={responsiveText}>Orlando, Florida</Text>
+            <Center>
+                <Flex alignItems='center'>
+                    <FaMapMarkerAlt />
+                    <Text fontSize={responsiveText} ml={2}>Orlando, Florida</Text>
+                </Flex>
+            </Center>
             <Center><Text fontSize={responsiveText}>Computer Science B.S.</Text></Center>
           </Grid>
           <Heading as="h1" ml={{xl:150}} letterSpacing="0.8rem" fontSize={{base:'5rem',md:'10rem','2xl':'13rem'}}>CAMERON<br/>WOOD</Heading>
-          <Flex w='full'>
-            <Spacer />
-            <a href="https://linkedin.com/in/cameron-wood-412708124"><SiLinkedin fontSize={responsiveText}/></a>
-          </Flex>
         </VStack>
       </Box>
     </ChakraProvider>
